bb8141706b181be3553cfe220a350b6e35fc6ef5
Christopher Hiller <boneskull@boneskull.com>
2018-11-18 21:34:19 -0800

    fix glob test to use --no-config

--- test/integration/glob.spec.js
+++ test/integration/glob.spec.js
@@ -179,7 +179,7 @@ function execMochaWith(validate) {
       node +
         ' "' +
         path.join('..', '..', '..', '..', 'bin', 'mocha') +
-        '" -R json-stream --config /dev/null ' +
+        '" -R json-stream --no-config ' +
         glob,
       {cwd: path.join(__dirname, 'fixtures', 'glob')},
       function(error, stdout, stderr) {
